ab943ee9b90bc25ae3e9f0f111cc1f7bac3571e0
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2025-12-03 23:10:10 +0530

    fixed inngest createFunction error

--- app/inngest/route.js
+++ app/inngest/route.js
@@ -1,18 +1,5 @@
 import { Inngest } from "inngest";
 import {serve} from "inngest/next";
-import { createFunction } from "inngest/next/function";
-
-
-// inggest functions demo 
-const hellowWorld = createFunction(
-    {id:"hellow-world"},
-    {event:"app/hellow-world"},
-    async({event,step})=>{
-        await step.sleep("wait-a-moment",3000)
-        return{message:`Hello World! from the event ${event.name}`}
-    }
-)
-
 export const inngest = new Inngest({
     id:"finance-tracker-app",
     name:"AI Finance Tracker App Inngest",
@@ -21,7 +8,6 @@ export const inngest = new Inngest({
 export default serve({
     client:inngest,
     functions:[
-        hellowWorld,
     ]
 })
 
